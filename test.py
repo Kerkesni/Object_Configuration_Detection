05c6b5de18cc203d81d716161b4c1f8774ceb932
Kerkesni <48165340+Kerkesni@users.noreply.github.com>
2020-03-08 19:29:04 +0100

    Added Euclidien Distance in test.py

--- test.py
+++ test.py
@@ -7,19 +7,9 @@ from PIL import Image
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
+from scipy.spatial.distance import euclidean
 
-dir = "./im_1/histograms"
-data = []
+a = np.genfromtxt('./im_1/histograms/sum.csv', delimiter=',')
+b = np.genfromtxt('./im_5/histograms/sum.csv', delimiter=',')
 
-for file in os.listdir(dir):
-    if file.endswith(".csv"):
-        data.append(np.genfromtxt(dir+'/'+file, delimiter=','))
-
-bigHisto = [0]*len(data[0])
-
-for histogram in data:
-    for index in range(len(histogram)):
-        bigHisto[index]+=histogram[index]/len(data)
-
-plt.plot(bigHisto)
-plt.savefig("a.png")
+print(euclidean(a, b))
